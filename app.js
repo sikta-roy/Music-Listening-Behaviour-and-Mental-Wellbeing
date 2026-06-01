5a75164a8ef9d896702ecbb230d51cc8086d3617
Sikta Roy <109263627+sikta-roy@users.noreply.github.com>
2026-06-01 23:28:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ─────────────────────────────────────────────
 // CONFIG — paste your Apps Script web app URL here
 // ─────────────────────────────────────────────
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwV-2-UfaAhGTSOxwyiN5Oxd8yijLGPpBafVllo7LJcNYHYnhbRoSHYHnyaGKnoZCA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyMQoCeNw1-sGT59-SUaeC8uaHvElty_CrPeI_I5SkjIhiWRlCdp4NFzYS901U2_OcEYg/exec';
 
 // ─────────────────────────────────────────────
 // QUESTIONNAIRE DATA
