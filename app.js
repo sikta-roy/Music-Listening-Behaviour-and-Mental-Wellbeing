10cd7a5f20fe876d97f46cf88648fd36bda916ea
Sikta Roy <109263627+sikta-roy@users.noreply.github.com>
2026-06-01 23:24:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -729,7 +729,7 @@ function renderThankYou() {
   const r1 = state.rrs1, r2 = state.rrs2;
   const dass_d = [2,4,9,12,15,16,20].reduce((s,i) => s + (d[i] || 0), 0) * 2;
   const dass_a = [1,3,6,8,14,18,19].reduce((s,i) => s + (d[i] || 0), 0) * 2;
-  const dass_s = [0,5,7,10,11,12,17].reduce((s,i) => s + (d[i] || 0), 0) * 2;
+  const dass_s = [0,5,7,10,11,13,17].reduce((s,i) => s + (d[i] || 0), 0) * 2;
   const pa1 = [0,2,4,8,9,11,13,15,16,18].reduce((s,i) => s + (p1[i] || 0), 0);
   const na1 = [1,3,5,6,7,10,12,14,17,19].reduce((s,i) => s + (p1[i] || 0), 0);
   const pa2 = [0,2,4,8,9,11,13,15,16,18].reduce((s,i) => s + (p2[i] || 0), 0);
